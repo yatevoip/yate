eb3f4c5adc7f332efb461bb2fb515ee9e9fb7f09
Manawyrm <git@tbspace.de>
2023-09-07 11:10:57 +0200

    miniwebrtc: add aarch64 support
    miniwebrtc fails to compile on aarch64/arm64 because the architecture isn't known
    this change adds the required configuration for arm64

--- libs/miniwebrtc/typedefs.h
+++ libs/miniwebrtc/typedefs.h
@@ -76,6 +76,9 @@
 //#define WEBRTC_ARCH_ARMEL
 #define WEBRTC_ARCH_32_BITS
 #define WEBRTC_ARCH_LITTLE_ENDIAN
+#elif defined(__aarch64__)
+#define WEBRTC_ARCH_64_BITS
+#define WEBRTC_ARCH_LITTLE_ENDIAN
 #elif defined(__mips__)
 #define WEBRTC_ARCH_32_BITS
 #define WEBRTC_BIG_ENDIAN
